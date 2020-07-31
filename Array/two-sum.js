5812fedba6d7f79bb3fdc16f661a4b75adac80fe
Tresor11 <tresor.moise2001@gmail.com>
2020-07-31 20:30:27 +0200

    solve two sum

---
+++ Array/two-sum.js
@@ -0,0 +1,17 @@
+//problem link
+// https://leetcode.com/problems/two-sum/
+
+var twoSum = function (nums, target) {
+  let hash = {};
+  let i = 0;
+  for (el of nums) {
+    let temp = target - el;
+    if (hash[temp.toString()] !== undefined) {
+      return [hash[temp.toString()], i];
+    } else {
+      hash[el.toString()] = i;
+    }
+    i++;
+  }
+  return [];
+};
