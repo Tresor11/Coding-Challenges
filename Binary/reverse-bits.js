b353dac73d914abce5a04d8fe16432eb490fe8a8
Tresor11 <tresor.moise2001@gmail.com>
2020-07-31 17:54:18 +0200

    solve the reverse bits challenge

---
+++ Binary/reverse-bits.js
@@ -0,0 +1,15 @@
+
+//problem link
+// https://leetcode.com/problems/reverse-bits/submissions/
+
+var reverseBits = function(n) {
+    let res=''
+    let count=32
+    while (count > 0){
+        temp= `${n&1}`
+        res+=temp
+        n = n >> 1
+        count--
+    }
+    return parseInt(res, 2)
+};
