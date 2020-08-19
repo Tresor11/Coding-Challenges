89bce18525c1e94fb0dd3450bc9c1167d6703b37
tresor <tresor.moise2001@gmail.com>
2020-08-19 13:47:13 +0200

    solve the insertion sort

---
+++ sorting/insertion_sort_1.rb
@@ -0,0 +1,20 @@
+def insertionSort1(n, arr)
+
+    el=arr[n-1]
+    samp=arr
+    cond=true
+    i=n-1
+    while cond
+    cond=false
+    if (samp[i-1] > el) && i != 0
+    samp[i]=samp[i-1]
+    cond=true 
+    else
+    samp[i]=el
+    cond=false
+    end
+    print samp.join(" ")
+    print "\n"
+    i -=1
+    end 
+    end
