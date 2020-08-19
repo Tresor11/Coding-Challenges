549f0e489db64faeb642744386f13dd5c72eea4c
tresor <tresor.moise2001@gmail.com>
2020-08-19 13:48:23 +0200

    run rubocop -a

--- sorting/insertion_sort_1.rb
+++ sorting/insertion_sort_1.rb
@@ -1,20 +1,21 @@
-def insertionSort1(n, arr)
+# frozen_string_literal: true
 
-    el=arr[n-1]
-    samp=arr
-    cond=true
-    i=n-1
-    while cond
-    cond=false
-    if (samp[i-1] > el) && i != 0
-    samp[i]=samp[i-1]
-    cond=true
+def insertionSort1(n, arr)
+  el = arr[n - 1]
+  samp = arr
+  cond = true
+  i = n - 1
+  while cond
+    cond = false
+    if (samp[i - 1] > el) && i != 0
+      samp[i] = samp[i - 1]
+      cond = true
     else
-    samp[i]=el
-    cond=false
+      samp[i] = el
+      cond = false
     end
-    print samp.join(" ")
+    print samp.join(' ')
     print "\n"
-    i -=1
-    end
+    i -= 1
+  end
     end
