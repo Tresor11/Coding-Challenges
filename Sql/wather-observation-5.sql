37fd2295f634d247e059e5be507fec026a0f2abc
Tresor <tresor.moise2001@gmail.com>
2021-01-22 09:46:51 +0200

    add a folder for sql challenges

---
+++ Sql/wather-observation-5.sql
@@ -0,0 +1,5 @@
+--Problem:
+--https://www.hackerrank.com/challenges/weather-observation-station-5/problem
+
+select top 1 city,LEN(city) city_len from station order by city_len, city;
+select top 1 city,LEN(city) city_len from station order by city_len desc,city;
