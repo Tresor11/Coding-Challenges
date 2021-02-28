efab3063ae22ac3a8801ba719e9bcff96f472e60
Tresor <tresor.moise2001@gmail.com>
2021-02-28 23:39:57 +0200

    binary three inorder traversal

---
+++ Hash Table/binary_three_inorder_traversal.rb
@@ -0,0 +1,10 @@
+#https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+def inorder_traversal(root)
+  return [] if root.nil?
+
+  result = [inorder_traversal(root.left)]
+  result << root.val
+  result << inorder_traversal(root.right)
+  result.flatten.compact
+end
