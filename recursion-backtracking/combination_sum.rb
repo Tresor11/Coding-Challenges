fad5eccc030c64e69a1eebbf170b6f42dc801b4b
Tresor <tresor.moise2001@gmail.com>
2021-03-07 22:21:16 +0200

    combination sum

---
+++ recursion-backtracking/combination_sum.rb
@@ -0,0 +1,21 @@
+def combination_sum(candidates, target)
+    result=[]
+    helper(result,[],candidates,target,index=0)
+    result
+end
+
+def helper(result,curr,candidates,target,index=0)
+    if target==0
+        result.push(curr.clone)
+    else
+        index.upto(candidates.size-1) do |i|
+            candidate=candidates[i]
+            next if target-candidate < 0
+            target-=candidate
+            curr.push(candidate)
+            helper(result,curr,candidates,target,i)
+            target+=candidate
+            curr.pop
+        end
+    end
+end
