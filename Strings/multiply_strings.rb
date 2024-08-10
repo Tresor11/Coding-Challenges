915fb0fc83a770a9132af9b802a33a47329b866b
Trésor Bireke <tresor.moise2001@gmail.com>
2024-08-10 21:10:12 +0400

    Create multiply_strings.rb

---
+++ Strings/multiply_strings.rb
@@ -0,0 +1,9 @@
+# @param {String} num1
+# @param {String} num2
+# @return {String}
+
+#link: https://leetcode.com/problems/multiply-strings/
+
+def multiply(num1, num2)
+    (num1.to_i * num2.to_i).to_s
+end
