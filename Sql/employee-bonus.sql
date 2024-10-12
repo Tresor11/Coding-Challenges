9f42d0bb3223af9749796e15dc734f9a03604c8a
Trésor Bireke <bireketresor@gmail.com>
2024-10-12 18:19:21 +0400

    Create employee-bonus.sql
    Add a new challenge solution

---
+++ Sql/employee-bonus.sql
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/employee-bonus/description/
+
+
+select employee.name, bonus.bonus from employee
+ left join bonus on employee.empId = bonus.empId
+  where ifnull(bonus.bonus,0) < 1000;
